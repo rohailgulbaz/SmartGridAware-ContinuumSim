726b90db02abb08e2eb9f1b114301c88e9b3ce66
rohailgulbaz <rohailgulbaz95@gmail.com>
2025-07-06 20:39:23 +0200

    Update DatacenterBrokerNormalFaaSCPriorityLessDC.java

--- SmartGridAware-ContinuumSim/src/my_package/DatacenterBrokerNormalFaaSCPriorityLessDC.java
+++ SmartGridAware-ContinuumSim/src/my_package/DatacenterBrokerNormalFaaSCPriorityLessDC.java
@@ -1,11 +1,3 @@
-/*
- * Title:        CloudSim Toolkit
- * Description:  CloudSim (Cloud Simulation) Toolkit for Modeling and Simulation of Clouds
- * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
- *
- * Copyright (c) 2009-2012, The University of Melbourne, Australia
- */
-
 package my_package;
 
 import java.io.BufferedReader;
